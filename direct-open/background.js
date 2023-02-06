9bec8fb9ce5ddb4e297669376b193c60bb39e1f8
shimo164 <shimo164@gmail.com>
2023-02-06 20:30:11 +0900

    Add icons. Refactor with JS coding style.

--- direct-open/background.js
+++ direct-open/background.js
@@ -1,4 +1,4 @@
-chrome.runtime.onInstalled.addListener(function (details) {
+chrome.runtime.onInstalled.addListener(() => {
   const parent = chrome.contextMenus.create({
     id: 'share',
     title: 'Open Lambda Page',
@@ -9,7 +9,6 @@ chrome.runtime.onInstalled.addListener(function (details) {
     parentId: parent,
     id: 'lambda-console',
     title: 'Lambda Console',
-    contexts: ['page'],
     contexts: ['selection'],
   });
 
@@ -28,44 +27,48 @@ chrome.runtime.onInstalled.addListener(function (details) {
   });
 });
 
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-  switch (info.menuItemId) {
-    case 'lambda-console':
-      menu_action(info, tab, 'lambda_console');
-      break;
-    case 'lambda-logs':
-      menu_action(info, tab, 'lambda_logs');
-      break;
-    case 'options':
-      chrome.runtime.openOptionsPage();
-      break;
-  }
-});
+async function genLambdaUrl(info, action) {
+  const region = await chrome.storage.local
+    .get(['key'])
+    .then((result) => result.key);
 
-function menu_action(info, tab, action) {
+  const fnName = info.selectionText;
+
+  let targetUrl = '';
+  if (action === 'lambda_console') {
+    targetUrl = `https://${region}.console.aws.amazon.com/lambda/home?region=${region}#/functions/${fnName}?tab=code`;
+  } else if (action === 'lambda_logs') {
+    targetUrl = `https://${region}.console.aws.amazon.com/cloudwatch/home?region=${region}#logStream:group=%252Faws%252Flambda%252F${fnName}`;
+  }
+  return targetUrl;
+}
+
+function menuAction(info, tab, action) {
   chrome.scripting.executeScript(
     {
       target: { tabId: tab.id },
-      function: gen_lambda_url,
+      function: genLambdaUrl,
       args: [info, action],
     },
     (injectionResults) => {
-      target_url = injectionResults[0].result;
-      chrome.tabs.create({ url: target_url });
+      const targetUrl = injectionResults[0].result;
+      chrome.tabs.create({ url: targetUrl });
     },
   );
 }
 
-async function gen_lambda_url(info, action) {
-  await chrome.storage.local.get(['key']).then((result) => {
-    region = result.key;
-  });
-
-  const fn_name = info.selectionText;
-
-  if (action === 'lambda_console') {
-    return `https://${region}.console.aws.amazon.com/lambda/home?region=${region}#/functions/${fn_name}?tab=code`;
-  } else if (action === 'lambda_logs') {
-    return `https://${region}.console.aws.amazon.com/cloudwatch/home?region=${region}#logStream:group=%252Faws%252Flambda%252F${fn_name}`;
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  switch (info.menuItemId) {
+    case 'lambda-console':
+      menuAction(info, tab, 'lambda_console');
+      break;
+    case 'lambda-logs':
+      menuAction(info, tab, 'lambda_logs');
+      break;
+    case 'options':
+      chrome.runtime.openOptionsPage();
+      break;
+    default:
+      break;
   }
-}
+});
