842897903a306ede70cc38a81bfe8e1b985dd2d4
shimo164 <shimo164@gmail.com>
2023-02-26 18:23:13 +0900

    Change to more specific variable names.

--- direct-open/options.js
+++ direct-open/options.js
@@ -1,30 +1,31 @@
-function Save() {
+function SaveRegion() {
   const value = document.getElementById('inputRegion').value;
 
-  chrome.storage.local.set({ key: value }).then(() => {});
+  chrome.storage.local.set({ region: value }).then(() => {});
 
-  const message1 = document.getElementById('saveDoneMessage1');
-  const message2 = document.getElementById('saveDoneMessage2');
+  const message1 = document.getElementById('saveRegionMessage1');
+  const message2 = document.getElementById('saveRegionMessage2');
   const now = new Date();
   message1.innerHTML = `Value is set to ${value}`;
   message2.innerHTML = now.toLocaleString();
 }
 
-function Load() {
-  chrome.storage.local.get('key', (items) => {
-    const savedRegion = items.key;
+function LoadRegion() {
+  chrome.storage.local.get('region', (items) => {
+    const savedRegion = items.region;
     if (typeof savedRegion !== 'undefined') {
       document.getElementById('inputRegion').value = savedRegion;
     }
   });
 }
+
 async function OpenLambda() {
   let targetUrl = '';
   const action = this.action;
 
   const region = await chrome.storage.local
-    .get(['key'])
-    .then((result) => result.key);
+    .get(['region'])
+    .then((result) => result.region);
 
   const fnName = document.getElementById('inputLambdaName').value;
 
@@ -37,9 +38,9 @@ async function OpenLambda() {
   chrome.tabs.create({ url: targetUrl });
 }
 
-document.addEventListener('DOMContentLoaded', Load);
+document.addEventListener('DOMContentLoaded', LoadRegion);
 
-document.getElementById('saveButton').addEventListener('click', Save);
+document.getElementById('saveButton').addEventListener('click', SaveRegion);
 
 document.getElementById('lambdaConsoleButton').addEventListener('click', {
   action: 'lambda_console',
